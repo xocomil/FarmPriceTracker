cbdcc5b310c6f93c74dab4beb8fe3ab0739a342f
xocomil <xocomil@gmail.com>
2021-12-11 13:43:12 -0700

    Provide the MainWindow through DI

--- FarmPriceTracker/App.xaml.cs
+++ FarmPriceTracker/App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace FarmPriceTracker;
+﻿using System.Windows;
+using AssemblyScanning;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FarmPriceTracker;
 
 /// <summary>
 ///   Interaction logic for App.xaml
 /// </summary>
 public partial class App {
+  private readonly ServiceProvider _serviceProvider;
+
+  public App() {
+    var services = new ServiceCollection();
+    ConfigureServices(services);
+    _serviceProvider = services.BuildServiceProvider();
+  }
+
+  private static void ConfigureServices(IServiceCollection services) {
+    services.RegisterServicesFromType<IAssemblyScanningMarker>();
+  }
+
+  private void OnStartup(object sender, StartupEventArgs e) {
+    var mainWindow = _serviceProvider.GetService<MainWindow>();
+
+    mainWindow?.Show();
+  }
 }
--- FarmPriceTracker/MainWindow.xaml.cs
+++ FarmPriceTracker/MainWindow.xaml.cs
@@ -1,12 +1,22 @@
-﻿using System.Windows;
+﻿using System;
+using System.Text.Json;
+using System.Windows;
+using AssemblyScanning;
+using FarmSimulator22Integrations.Parsers;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace FarmPriceTracker; 
+namespace FarmPriceTracker;
 
 /// <summary>
 ///   Interaction logic for MainWindow.xaml
 /// </summary>
+[Inject(serviceLifetime: ServiceLifetime.Singleton, provideFor: typeof(MainWindow))]
 public partial class MainWindow : Window {
   public MainWindow() {
     InitializeComponent();
+
+    var test = Fs22Map.CreateInstance();
+
+    Console.WriteLine($"map: {JsonSerializer.Serialize(test.Map)}");
   }
 }
