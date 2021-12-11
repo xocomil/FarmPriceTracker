b820e5807d75d4bd07db6c225817de9c4fc49b0b
xocomil <xocomil@gmail.com>
2021-12-11 11:52:41 -0700

    add Inject() attribute

---
+++ AssemblyScanning/InjectAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AssemblyScanning;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class InjectAttribute: Attribute {
+  public ServiceLifetime ServiceLifetime { get; init; }
+  public Type ProvideForInterface { get; init; }
+
+  public InjectAttribute(Type provideForInterface = null, ServiceLifetime serviceLifetime = ServiceLifetime.Transient) {
+    ServiceLifetime = serviceLifetime;
+
+    if ( provideForInterface is { IsInterface: false } ) {
+      throw new ArgumentException("ProvideForInterface must be an interface.", nameof(provideForInterface));
+    }
+
+    ProvideForInterface = provideForInterface;
+  }
+}
