d4f5cb00890d1145e6e636dd522872eea974cfb3
xocomil <xocomil@gmail.com>
2021-12-14 16:31:02 -0700

    added data folder setting and validation icons

--- FarmPriceTracker/ViewModels/SettingsViewModel.cs
+++ FarmPriceTracker/ViewModels/SettingsViewModel.cs
@@ -1,16 +1,57 @@
-﻿using System.Reactive;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Windows;
 using AssemblyScanning;
 using FarmPriceTracker.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using ReactiveUI;
+using ReactiveUI.Validation.Contexts;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Helpers;
 using Splat;
 
 namespace FarmPriceTracker.ViewModels;
 
 [Inject(serviceLifetime: ServiceLifetime.Singleton, provideFor: typeof(SettingsViewModel))]
-public class SettingsViewModel {
+public class SettingsViewModel : ReactiveValidationObject {
+  private readonly ObservableAsPropertyHelper<bool> _dataFolderValid;
+  private string _dataFolder = @"c:\test\test\test";
+
+  public SettingsViewModel() {
+    this.ValidationRule(
+      vm => vm.DataFolder,
+      folder => !string.IsNullOrWhiteSpace(folder),
+      "You must specify the location of the data folder."
+    );
+
+    this.ValidationRule(vm => vm.DataFolder, Directory.Exists, "The data folder does not exist.");
+
+    _dataFolderValid = this.IsValid()
+      // .ObserveOn(RxApp.MainThreadScheduler)
+      .Do(model => Trace.WriteLine($"valid: {model}"))
+      // .Where(valid => !valid)
+      .Select(valid => !valid && (GetErrors(nameof(DataFolder)) as IEnumerable<string>).Any())
+      .Do(model => Trace.WriteLine($"after select: {model}"))
+      .ToProperty(this, x => x.DataFolderValid, scheduler: RxApp.MainThreadScheduler);
+
+    this.WhenAnyValue(x => x.DataFolderValid).Subscribe(valid => Trace.WriteLine($"whenanyvalue valid: {valid}"));
+  }
+
+  public bool DataFolderValid => _dataFolderValid.Value;
+
+  public string DataFolder {
+    get => _dataFolder;
+    set => this.RaiseAndSetIfChanged(ref _dataFolder, value);
+  }
+
   public ReactiveCommand<Window, Unit> CloseSettings { get; } = ReactiveCommand.Create<Window>(CloseSettingsWindow);
+
+  public ValidationContext ValidationContext { get; } = new();
 
   private static void CloseSettingsWindow(Window window) {
     window.Close();
@@ -19,6 +60,6 @@ public class SettingsViewModel {
   public static void OpenSettings() {
     var settingsWindow = Locator.Current.GetService<SettingsWindow>();
 
-    settingsWindow?.Show();
+    settingsWindow?.ShowDialog();
   }
 }
